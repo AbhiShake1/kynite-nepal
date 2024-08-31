552d76fc45f239776b02083ecf04b08b3943cb5c
abhishake1 <abhi9848038283@gmail.com>
2024-08-31 08:26:44 +0000

    feat: make nav active per page path

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,6 +1,18 @@
+"use client"
+
+import { cn } from "@/lib/utils"
 import Image from "next/image"
+import { usePathname } from "next/navigation"
+
+const links = [
+  { name: "Home", href: "/" },
+  { name: "Shop", href: "/shop" },
+  { name: "About", href: "/about" },
+  { name: "Contact", href: "/contact" },
+]
 
 export function Header() {
+  const pathname = usePathname()
   return <header className="px-4 lg:px-6 h-14 flex items-center backdrop-blur-sm bg-white/30 border-b border-white/20 sticky top-0 z-50">
     <a className="flex items-center justify-center" href="#">
       <Image
@@ -13,18 +25,17 @@ export function Header() {
       <span className="ml-2 text-2xl font-bold text-blue-900">Kyanite Nepal</span>
     </a>
     <nav className="ml-auto flex gap-4 sm:gap-6 text-primary">
-      <a className="text-sm font-medium hover:text-blue-700 transition-colors" href="/">
-        Home
-      </a>
-      <a className="text-sm font-medium hover:text-blue-700 transition-colors" href="/shop">
-        Shop
-      </a>
-      <a className="text-sm font-medium hover:text-blue-700 transition-colors" href="/about">
-        About
-      </a>
-      <a className="text-sm font-medium hover:text-blue-700 transition-colors" href="/contact">
-        Contact
-      </a>
+      {links.map((link) => (
+        <a
+          key={link.name}
+          className={cn("text-sm font-medium hover:text-blue-700 transition-colors", {
+            "text-blue-700": pathname === link.href,
+          })}
+          href={link.href}
+        >
+          {link.name}
+        </a>
+      ))}
     </nav>
   </header>
 }
